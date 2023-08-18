74e5b6e96ed4a8add2c71dd8469dd1b59a306f4f
armoredchimp <mattkasmir@gmail.com>
2023-08-17 19:55:28 -0700

    end of 8/17

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,25 @@
-console.log('Express Tutorial')
+const http = require("http");
+const server = http.createServer((req, res) => {
+  // console.log(req.url);
+  const url = req.url;
+  //home page
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>home page<h1>");
+    res.end();
+  }
+  //about page
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about page<h1>");
+    res.end();
+  }
+  //404
+  else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>Fail!<h1>");
+    res.end();
+  }
+});
+
+server.listen(5000);
