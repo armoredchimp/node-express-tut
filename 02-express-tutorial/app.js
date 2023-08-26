20b52db3be1cb26afa9c4b96b5703f7293ef20ce
armoredchimp <mattkasmir@gmail.com>
2023-08-25 22:24:45 -0700

    8/25 eve

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,45 +1,24 @@
-const http = require("http");
-const { readFileSync } = require("fs");
-
-const homePage = readFileSync("./navbar-app/index.html");
-const homeStyles = readFileSync("./navbar-app/styles.css");
-const homeImage = readFileSync("./navbar-app/logo.svg");
-const homeLogic = readFileSync("./navbar-app/browser-app.js");
-
-const server = http.createServer((req, res) => {
-  // console.log(req.url);
-  const url = req.url;
-  console.log(url);
-  //home page
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(homePage);
-    res.end();
-  }
-  //about page
-  else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>about page<h1>");
-    res.end();
-  } else if (url === "/styles.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    res.write(homeStyles);
-    res.end();
-  } else if (url === "/logo.svg") {
-    res.writeHead(200, { "content-type": "image/svg+xml" });
-    res.write(homeImage);
-    res.end();
-  } else if (url === "/browser-app.js") {
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.write(homeLogic);
-    res.end();
-  }
-  //404
-  else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>Fail!<h1>");
-    res.end();
-  }
+const express = require("express");
+const app = express();
+const { products } = require("./data");
+app.get("/", (req, res) => {
+  res.send('<h1>HomePage</h1><a href="/api/products">products</a>');
+});
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+app.listen(5000, () => {
+  console.log("Moo!");
 });
 
-server.listen(5000);
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
