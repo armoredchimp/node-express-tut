368a6304a6b80d7dc2942ebcd5f963eeb1cf1e79
armoredchimp <mattkasmir@gmail.com>
2023-09-20 19:40:21 -0700

    9/20

---
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
+  const id = new Date().getDate();
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: "user created", token });
+  res.send("Fake Login/Register/Signup Route");
+};
+
+const dashboard = async (req, res) => {
+  console.log(req.headers);
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello, John Doe`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
+};
+
+module.exports = { login, dashboard };
