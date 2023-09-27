31ddc7383b54ca2a82efc2814cb30440dc8b5847
armoredchimp <mattkasmir@gmail.com>
2023-09-26 21:52:17 -0700

    9/26

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,7 +1,7 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   // const { name, email, password } = req.body;
@@ -13,7 +13,10 @@ const register = async (req, res) => {
   //   throw new BadRequestError("Please provide name, email, and password");
   // }
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
+    expiresIn: "30d",
+  });
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 const login = async (req, res) => {
   res.send("login user");
